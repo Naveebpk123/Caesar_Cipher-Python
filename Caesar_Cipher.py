c8cb73c27305c7985001ff5ca199fc5c70274d54
Naveeb Pacheerikkuth <naveebpk132@gmail.com>
2026-02-27 19:41:03 +0530

    Update Caesar_Cipher.py
    Added support for uppercase letters, numbers, symbols

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -3,44 +3,54 @@ import string
 def caesar():
     alphabet = list(string.ascii_lowercase)
     choice = input("decrypt or encrypt?\n").lower()
-    message = input("Enter the message\n").lower()
+    message = input("Enter the message\n")
     shift = int(input("Enter the shift number\n"))
 
     def encrypt(original_text, shift_amount):
         coded_text = ""
         for letter in original_text:
-            if letter in alphabet:
-                shifted_position = (alphabet.index(letter) + shift_amount) % len(alphabet)
-                coded_text += alphabet[shifted_position]
+            if letter.lower() in alphabet:
+                index = alphabet.index(letter.lower())
+                shifted_position = (index + shift_amount) % len(alphabet)
+                new_char = alphabet[shifted_position]
+                
+                if letter.isupper():
+                    coded_text += new_char.upper()
+                else:
+                    coded_text += new_char
             else:
                 coded_text += letter
-        print(f"Your encrypted message is:\n{coded_text}")
+        print(f"Your encrypted message is\n {coded_text}")
 
     def decrypt(original_text, shift_amount):
-        decoded_text = ""
+        coded_text = ""
         for letter in original_text:
-            if letter in alphabet:
-
-                shifted_position = (alphabet.index(letter) - shift_amount) % len(alphabet)
-                decoded_text += alphabet[shifted_position]
+            if letter.lower() in alphabet:
+                index = alphabet.index(letter.lower())
+                shifted_position = (index - shift_amount) % len(alphabet)
+                new_char = alphabet[shifted_position]
+                
+                if letter.isupper():
+                    coded_text += new_char.upper()
+                else:
+                    coded_text += new_char
             else:
-                decoded_text += letter
-        print(f"Your decrypted message is:\n{decoded_text}")
+                coded_text += letter
+        print(f"Your decrypted message is\n {coded_text}")
 
     if choice == "decrypt":
         decrypt(message, shift)
     elif choice == "encrypt":
         encrypt(message, shift)
 
-print("Welcome to Caesar Cipher.")
-direction = input("Type 'quit' to exit or press Enter to start: ").lower()
-
-if direction != "quit":
+direction = input("Welcome to Caesar Cipher. \nType quit to exit. \nClick Enter to start")
+if not direction == "quit":
     while True:
         caesar()
-        choice2 = input("\nType 'quit' to exit or press Enter to continue: ").lower()
+        choice2 = input("Type quit to exit.\nType Enter to continue")
         if choice2 == "quit":
             break
+
 
 
 
